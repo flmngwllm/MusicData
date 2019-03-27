ea42c523fae655b56fca68e029389c35a4637e8e
William Fleming <chaosmegaman@Williams-MacBook-Pro.local>
2019-03-27 16:35:01 -0400

    creating database conneection

---
+++ src/com/company/model/Datasource.java
@@ -0,0 +1,26 @@
+package com.company.model;
+
+public class Datasource {
+
+    public static final String DB_NAME = "music.db";
+    public static final String CONNECTION_STRING = "jdbc:sqlite:/Users/chaosmegaman/IdeaProjects/Music" + DB_NAME;
+
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUM_ID = "_id";
+    public static final String COLUMN_ALBUM_NAME = "name";
+    public static final String COLUMN_ALBUM_ARTIST = "artist";
+
+    public static final String TABLE_ARTISTS = "artists";
+    public static final String COLUMN_ARTISTS_ID = "_id";
+    public static final String COLUMN_ARTISTS_NAME = "name";
+
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONGS_TRACK = "track";
+    public static final String COLUMN_SONGS_TITLE = "title";
+    public static final String COLUMN_SONGS_ALBUM = "album";
+    
+
+}
+
+
+
